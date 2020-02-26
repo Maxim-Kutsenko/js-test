349ec2cabceb3dab254a398eee22a049ce9ac929
Maxim-kutsenko <envenom2610>
2020-02-26 14:05:19 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('info-header'),
-        tabContent = document.querySelectorAll('info-tabcontent');
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
